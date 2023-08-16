13c2b4a68949c6be737b467b4553479104e3f37e
Abir Mokaddem <amokaddem@archeti.com>
2023-08-16 14:12:50 -0400

    [#25827] FIX auth_session_timeout: Fix too many redirect url - add checking if file exist

--- auth_session_timeout/models/res_users.py
+++ auth_session_timeout/models/res_users.py
@@ -5,6 +5,7 @@
 import logging
 
 from os.path import getmtime
+from os.path import isfile
 from time import time
 from os import utime
 
@@ -70,8 +71,10 @@ class ResUsers(models.Model):
         if deadline is not False:
             path = http.root.session_store.get_session_filename(session.sid)
             try:
-
-                expired = getmtime(path) < deadline
+                # FIX Error too many redirects url
+                # Check if file exists before getting mtime
+                if isfile(path):
+                    expired = getmtime(path) < deadline
             except OSError:
                 _logger.exception(
                     'Exception reading session file modified time.',
